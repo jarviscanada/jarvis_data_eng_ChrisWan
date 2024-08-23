b13d1f46c0cb324c74039b5a2742e63d2631e4b9
Chris Wan <chrisxinranwan@gmail.com>
2024-08-23 23:47:41 +0000

    Finished Java HTTP & JSON

---
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/API/AlphaVantageRESTAPI.java
@@ -0,0 +1,36 @@
+package ca.jrvs.apps.jdbc.API;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class AlphaVantageRESTAPI {
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        String apiKey = dotenv.get("API_KEY");
+        String symbol = "MSFT";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://alpha-vantage.p.rapidapi.com/query?function=GLOBAL_QUOTE&symbol="+symbol+"&datatype=json"))
+                .header("X-RapidAPI-Key", apiKey)
+                .header("X-RapidAPI-Host", "alpha-vantage.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        try {
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
